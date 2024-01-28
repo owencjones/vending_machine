5c0fcfd16f98a881fe1d38d80c81aff0634372ca
Owen Jones <owenxjones@pm.me>
2024-01-28 13:31:21 +0000

    Remove type hinting that FastAPI hates

--- vending_machine/database.py
+++ vending_machine/database.py
@@ -16,7 +16,7 @@ SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 Base = declarative_base()
 
 
-def get_db() -> Generator[Session, None, None]:
+def get_db():
     db = SessionLocal()
     try:
         yield db
