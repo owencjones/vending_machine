d8e83fea630a8fde52e854fc020e61ae5aac42b4
Owen Jones <owenxjones@pm.me>
2024-01-31 14:20:35 +0000

    WIP Tests

--- tests/controllers/test_machine.py
+++ tests/controllers/test_machine.py
@@ -3,106 +3,44 @@ from fastapi.testclient import TestClient
 from sqlalchemy.ext.asyncio import AsyncSession
 
 from vending_machine.main import main
-from vending_machine.models.api_messages import ApiMessage
 from vending_machine.models.product import Product
 from vending_machine.models.user import UserWithoutPassword
 from vending_machine.models.session import UserSession
-from vending_machine.database import get_db
 from vending_machine.models.session_product import SessionProduct
 from datetime import datetime, timedelta
+from vending_machine.database import SessionLocal
+
+from tests.fixtures import *  # noqa
+
+from typing import Tuple
 
 
-client = TestClient(main())
+
+def test_get_products(test_client: TestClient, test_products: Tuple[Product, Product]) -> None:
+    product1, product2 = test_products
+
+    # Make a request to get the products
+    response = test_client.get("/machine/products", dependencies=[(UserWithoutPassword, {"id": 1})])
+    assert response.status_code == 200
+    assert response.json() == [product1.model_dump(), product2.model_dump()]
 
 
-async def test_get_products():
-    async with AsyncSession() as session:
-        async with session.begin():
-            # Create a user session
-            user_session = UserSession(user_id=1, expiry_time=datetime.now() + timedelta(hours=1))
-            session.add(user_session)
-            session.commit()
-
-            # Create some products
-            product1 = Product(name="Product 1", price=10)
-            product2 = Product(name="Product 2", price=20)
-            session.add_all([product1, product2])
-            session.commit()
-
-            # Add products to the user session
-            user_session.products.extend([product1, product2])
-            session.commit()
-
-            # Make a request to get the products
-            response = await client.get("/machine/products", dependencies=[(UserWithoutPassword, {"id": 1})])
-            assert response.status_code == 200
-            assert response.json() == [product1.dict(), product2.dict()]
+def test_deposit(test_client: TestClient) -> None:
+    ...
 
 
-async def test_deposit():
-    async with AsyncSession() as session:
-        async with session.begin():
-            # Create a user session
-            user_session = UserSession(user_id=1, expiry_time=datetime.now() + timedelta(hours=1))
-            session.add(user_session)
-            session.commit()
-
-            # Make a request to deposit
-            response = await client.post(
-                "/machine/deposit", json={"amount": 10}, dependencies=[(UserWithoutPassword, {"id": 1})]
-            )
-            assert response.status_code == 200
-            assert response.json() == ApiMessage(message="Deposited successfully", success=True).dict()
-
-            # Check the deposited amount in the user session
-            assert user_session.deposited_amount == 10
+async def test_buy_product(test_client: TestClient) -> None:
+    ...
 
 
-async def test_buy_product():
-    async with AsyncSession() as session:
-        async with session.begin():
-            # Create a user session
-            user_session = UserSession(user_id=1, expiry_time=datetime.now() + timedelta(hours=1))
-            session.add(user_session)
-            session.commit()
+async def test_reset(test_client: TestClient, test_session: SessionLocal, test_products: Tuple[Product, Product]) -> None:
+    assert len(test_session.query(UserSession).first().products) == 2
 
-            # Create a product
-            product = Product(name="Product 1", price=10)
-            session.add(product)
-            session.commit()
+    response = test_client.post("/machine/reset", dependencies=[(UserWithoutPassword, {"id": 1})])
 
-            # Make a request to buy the product
-            response = await client.get("/machine/buy/1/1", dependencies=[(UserWithoutPassword, {"id": 1})])
-            assert response.status_code == 200
-            assert response.json() == product.dict()
+    assert response.status_code == 200
 
-            # Check the deposited amount in the user session
-            assert user_session.deposited_amount == 0
-
-            # Check the products in the user session
-            assert user_session.products == [product]
-
-
-async def test_reset():
-    async with AsyncSession() as session:
-        async with session.begin():
-            user_session = UserSession(user_id=1, expiry_time=datetime.now() + timedelta(hours=1))
-            session.add(user_session)
-            session.commit()
-
-            # Create some session products
-            session_product1 = SessionProduct(user_id=1, product_id=1)
-            session_product2 = SessionProduct(user_id=1, product_id=2)
-            session.add_all([session_product1, session_product2])
-            session.commit()
-
-            # Make a request to reset
-            response = await client.get("/machine/reset", dependencies=[(UserWithoutPassword, {"id": 1})])
-            assert response.status_code == 200
-            assert response.json() == True
-
-            # Check that the session products are deleted
-            assert session.query(SessionProduct).filter(SessionProduct.user_id == 1).count() == 0
+    assert len(test_session.query(UserSession).first().products) == 0
 
 
 if __name__ == "__main__":
--- tests/fixtures.py
+++ tests/fixtures.py
@@ -4,8 +4,12 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.pool import StaticPool
 
-from vending_machine.database import Base
-from vending_machine.main import main, get_db
+from vending_machine.database import Base, SessionLocal, get_db
+from vending_machine.main import main
+
+from datetime import datetime, timedelta
+from vending_machine.models.product import Product
+from vending_machine.models.session import UserSession
 
 
 @pytest.fixture
@@ -38,9 +42,34 @@ def test_client() -> TestClient:
 
 
 @pytest.fixture
+def test_session(test_client: TestClient):
+    yield test_client.app.dependency_overrides[get_db]()
+
+
+@pytest.fixture
 def user_data() -> dict:
     return {
         "username": "testuser",
         "role": "BUYER",
         "password": "testpassword",
     }
+
+
+@pytest.fixture
+def test_products(test_session: SessionLocal) -> tuple[Product, Product]:
+    # Create a user session
+    user_session = UserSession(user_id=1, expiry_time=datetime.now() + timedelta(hours=1))
+    test_session.add(user_session)
+    test_session.commit()
+
+    # Create some products
+    product1 = Product(name="Product 1", price=10)
+    product2 = Product(name="Product 2", price=20)
+    test_session.add_all([product1, product2])
+    test_session.commit()
+
+    # Add products to the user session
+    user_session.products.extend([product1, product2])
+    test_session.commit()
+
+    return product1, product2
